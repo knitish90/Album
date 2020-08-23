97052b0e2f39d0c2167a52bd5ad6b42b908a07cf
Nitish.kumar <nitish.kumar@landmarkgroup.in>
2020-08-23 20:53:38 +0530

    basic folder structure added

---
+++ Albums/AlbumList/Protocol/AlbumListProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  TrackListProtocol.swift
+//  Albums
+//
+//  Created by Nitish.kumar on 23/08/20.
+//  Copyright © 2020 Nitish.kumar. All rights reserved.
+//
+
+import Foundation
+
+protocol AlbumListViewModelProtocol {
+    
+}
---
+++ Albums/AlbumList/View/AlbumListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Albums
+//
+//  Created by Nitish.kumar on 23/08/20.
+//  Copyright © 2020 Nitish.kumar. All rights reserved.
+//
+
+import UIKit
+
+class AlbumListViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
